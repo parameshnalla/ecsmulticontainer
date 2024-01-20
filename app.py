dc038f98bb997e0869d283b583dd2b67cd7a80bb
Paramesh Nalla <parameshnalla@Parameshs-MacBook-Pro.local>
2024-01-20 00:38:45 -0500

    Fixed the arguments order

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import redis
 import socket
 
 app = Flask(__name__)
-redis_client = redis.Redis(host='redis', port=6379, db=0)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def increment():
